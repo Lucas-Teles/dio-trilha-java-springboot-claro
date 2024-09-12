132d152eceaf38c24e835415756d7440a289ff76
Lucas Teles <lucasouzateles@gmail.com>
2024-09-11 23:12:30 -0300

    feat: adicionando novo projeto

---
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,53 @@
+package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"Levi", "Rafael", "Lucas", "Pedro", "Laura", "Renata"};
+        for (String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Levi", "Rafael", "Lucas", "Pedro", "Nartan", "Flavio", "Laura", "Renata", "Andrea"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+            double salarioBase = 2000.0;
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+
+    static double valorPretendido(){
+        double valor = ThreadLocalRandom.current().nextDouble(1800, 2200);
+        return Math.round(valor * 100.0) / 100.0;
+    }
+
+}
